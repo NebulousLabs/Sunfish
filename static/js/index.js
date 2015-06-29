b626c29e5aa2ce0a570fc8ed941335d8b534c719
Aaron Gunderson <aaron@agundy.com>
2015-06-29 19:21:06 -0400

    Switched td id to something unique

--- static/js/index.js
+++ static/js/index.js
@@ -6,14 +6,14 @@ var fileDownload = function(siafile){
         text: siafile.filename
     });
 
-    $("#" + siafile.title).append($a);
+    $("#" + siafile.Id).append($a);
 };
 
 $.get("/api/siafile/", function(data){
     for (var i = 0; i < data.length; i++){
         var siafile = data[i];
         var uploadDate = new Date(siafile.uploadedTime);
-        $('.siafile-table').append("<tr><td>" + siafile.title + "</td><td>" + siafile.description + "</td><td>" + siafile.tags + "</td><td>" + uploadDate.toLocaleString() + "<td id='" + siafile.title + "'></td></tr>");
+        $('.siafile-table').append("<tr><td>" + siafile.title + "</td><td>" + siafile.description + "</td><td>" + siafile.tags + "</td><td>" + uploadDate.toLocaleString() + "<td id='" + siafile.Id + "'></td></tr>");
         fileDownload(siafile);
 
     }
