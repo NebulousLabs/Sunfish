d9a2485dfa23c952caa8573249e9382d6476a124
Aaron Gunderson <aaron@agundy.com>
2015-07-26 11:56:29 -0400

    Added flag for dbName

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 func main() {
 	// Add flag for listening on a different port
 	port := flag.String("port", "10181", "Port to listen on.")
+	dbName := flag.String("dbName", "sunfish", "Mongo DB to use")
 	logDir := flag.String("logDir", "logs", "Directory to put log file in.")
 	flag.Parse()
 
-	sf := NewSunfish(*logDir)
+	sf := NewSunfish(*logDir, *dbName)
 	defer sf.Close()
 	err := http.ListenAndServe(":"+*port, sf.Router)
 	if err != nil {
