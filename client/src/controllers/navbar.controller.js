424ab474ba16a64dee840ec8ecdf115838ec1d0b
Aaron Gunderson <aaron@agundy.com>
2015-08-02 14:11:44 -0400

    Indicate current page

--- client/src/controllers/navbar.controller.js
+++ client/src/controllers/navbar.controller.js
@@ -3,17 +3,21 @@
 app.controller('NavbarCtrl', function($scope, $location) {
     $scope.menu = [{
         'title': 'Home',
-        'link': '/#/'
+        'link': '/'
     },{
         'title': 'Browse',
-        'link': '/#/browse/'
+        'link': '/browse/'
     },{
         'title': 'Search',
-        'link': '/#/search/'
+        'link': '/search/'
     },{
         'title': 'Upload',
-        'link': '/#/upload/'
+        'link': '/upload/'
     }];
+
+    $scope.isActive = function(pagelink){
+        return pagelink == $location.url();
+    }
 
     $scope.toggleTopbar = function(){
       Foundation.libs.topbar.toggle($('.top-bar'));
