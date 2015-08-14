2252a844b661e34be8a86e424b79c9eae08d9179
Aaron Gunderson <aaron@agundy.com>
2015-08-14 12:18:47 -0400

    Wrote tests for creating users

--- userHandlers_test.go
+++ userHandlers_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -15,25 +15,26 @@ var (
 	dbName = "testSunfish"
 	sf     *Sunfish
 	url    string
+	client http.Client
 )
+
+func addUser(userInfo *UserSignup) (resp *http.Response, err error) {
+	marshalledJson, _ := json.Marshal(userInfo)
+	req, err := http.NewRequest("POST", url+"user/", bytes.NewReader(marshalledJson))
+	req.Header.Set("Content-Type", "application/json")
+
+	return client.Do(req)
+}
 
 func TestAddUser(t *testing.T) {
 	var user User
-	userString := `{
-		"username": "testUser",
-		"email": "test@example.com",
-		"fullname": "Test User",
-		"password": "password1234"
-	}`
-
-	req, err := http.NewRequest("POST", url+"user/", strings.NewReader(userString))
-	req.Header.Set("Content-Type", "application/json")
-
-	if err != nil {
-		sf.logger.Println(err)
+	userInfo := UserSignup{
+		Email:    "test@example.com",
+		Username: "testUser",
+		Fullname: "Test User",
+		Password: "password1234",
 	}
-
-	res, err := http.DefaultClient.Do(req)
+	res, err := addUser(&userInfo)
 
 	if res.StatusCode != http.StatusCreated {
 		t.Errorf("Error: expected %d. Received: %d", http.StatusCreated, res.StatusCode)
@@ -45,7 +46,61 @@ func TestAddUser(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error unmarshalling JSON into user. %s %s", body, err)
 	}
+}
 
+// TestBlankUserName test that servers sends bad request with no password field
+func TestBlankUserName(t *testing.T) {
+	userInfo := UserSignup{
+		Email:    "test1@example.com",
+		Username: "",
+		Fullname: "Test User 1",
+		Password: "password1234",
+	}
+	res, err := addUser(&userInfo)
+
+	if err != nil {
+		t.Errorf("Error adding user: %s", userInfo)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("Error: expected %d. Received: %d", http.StatusBadRequest, res.StatusCode)
+	}
+}
+
+func TestBlankEmail(t *testing.T) {
+	userInfo := UserSignup{
+		Email:    "",
+		Username: "testUser2",
+		Fullname: "Test User 2",
+		Password: "password1234",
+	}
+	res, err := addUser(&userInfo)
+
+	if err != nil {
+		t.Errorf("Error adding user: %s", userInfo)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("Error: expected %d. Received: %d", http.StatusBadRequest, res.StatusCode)
+	}
+}
+
+func TestBlankPassword(t *testing.T) {
+	userInfo := UserSignup{
+		Email:    "test3@example.com",
+		Username: "testUser3",
+		Fullname: "Test User 3",
+		Password: "",
+	}
+	res, err := addUser(&userInfo)
+
+	if err != nil {
+		t.Errorf("Error adding user: %s", userInfo)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("Error: expected %d. Received: %d", http.StatusBadRequest, res.StatusCode)
+	}
 }
 
 func TestMain(m *testing.M) {
